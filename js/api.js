a17ea213ba7ddb5bb36d6c3c0c0e28294883158e
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-09-11 19:19:36 +0600

    weather added

---
+++ js/api.js
@@ -0,0 +1,23 @@
+// api key
+const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('weather', temperature.weather[0].main);
+
+    console.log(temperature);
+}
